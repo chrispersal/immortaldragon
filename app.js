84974bdcf13e447f0283447663e9414d16f36b56
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-07-18 15:22:29 +0100

    Making the counter display properly.

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let damage = 0; // Start cookies count at zero
+let damage = 0; // start count at zero
 setInterval(function () {
   damage = damage + 1;
   console.log("🗡️", damage);
-}, 1000); // 1000 milliseconds == 1 second
+  damUpdate();
+}, 1000); // intervals count in ms
+
+const clickCount = document.getElementById("bigDragon");
+
+clickCount.addEventListener("click", clickAttack);
+
+function damUpdate() {
+  const damDis = document.querySelector("#damDis");
+  damDis.textContent = damage + " DMG";
+}
