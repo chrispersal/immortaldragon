f8f660acfb9a5e1f393b2057453ae183d7a2e745
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-07-18 16:05:01 +0100

    Added hurt sound.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 let damage = 0; // start count at zero
 setInterval(function () {
   damage = damage + 1;
-  console.log("🗡️", damage);
+  console.log("💥", damage);
   damUpdate();
 }, 1000); // intervals count in ms
 
-const clickCount = document.getElementById("bigDragon");
+function clickHit() {
+  damage++;
+  console.log("🗡️", damage);
+  damUpdate();
+  soundPlay();
+}
 
-clickCount.addEventListener("click", clickAttack);
+const clickAttack = document.querySelector(".bigDragon");
+clickAttack.addEventListener("click", clickHit);
 
 function damUpdate() {
   const damDis = document.querySelector("#damDis");
   damDis.textContent = damage + " DMG";
+}
+
+// HURT SOUND EFFECT
+function soundPlay() {
+  var audio = new Audio("./hurtsfx.wav");
+  audio.volume = 0.6;
+  audio.play();
 }
