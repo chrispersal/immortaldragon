16ce7c7d4987d09595ad83f9ee1adfdf3e83cdd0
Christian Richard Fairhall Smith <fairhallsmith@gmail.com>
2024-07-19 15:36:03 +0100

    My dragon escaped.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let damage = parseInt(localStorage.getItem("dmgdealt")) || 0; // use locally stored number or start count at zero
 setInterval(function () {
   damage = damage + 1;
-  console.log("💥", damage);
+  console.log("💥", damage); // discrete symbols for click damage vs. auto damage
   damUpdate();
 }, 1000); // intervals count in ms
 
+// CLICK ATTACK
 function clickHit() {
   damage++;
   console.log("🗡️", damage);
@@ -28,7 +29,7 @@ function soundPlay() {
   audio.play();
 }
 
-// Reset stats
+// RESET COUNT
 function resetDMG() {
   damage = 0;
   damUpdate(); // Clear damage amount local storage
@@ -45,3 +46,14 @@ fetch("https://cookie-upgrade-api.vercel.app/api/upgrades")
   .then(function (upgrades) {
     console.log(upgrades);
   });
+function displayUpgrades(upgrades) {}
+
+let upg1 = document.getElementById("upg1");
+buy1.addEventListener("click", upg1);
+
+// TO DO:
+// Pull values from the API and connect to the buttons so clicking purchases one of the upgrade in question, deducts from total damage pool and adds the extra amount to the DPS
+// Change name for each upgrade from the cookie-themed name to something more appropriate (using DOM Manipulation?)
+// Display the cost and amount of each upgrade item in the button text
+// Add a DPS display that shows the DPS, affected by any upgrades
+// Make the reset button also wipe any purchased upgrades
